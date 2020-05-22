3a12207d6270e18db48b4b0ab6a8137ed219ff05
Thomas Angelini <thomas.angelini@studio.unibo.it>
2020-05-22 17:02:08 +0200

    Log Msg for Gui

--- src/main/java/pcd/ass03/ex01/messages/LogMsg.java
+++ src/main/java/pcd/ass03/ex01/messages/LogMsg.java
@@ -1,4 +1,20 @@
 package pcd.ass03.ex01.messages;
 
+/*
+ * log to notify Gui
+ */
 public final class LogMsg implements Message {
+
+    /*
+     * log is the string message
+     */
+    private final String log;
+
+    public LogMsg(final String log) {
+        this.log = log;
+    }
+
+    public String getLog() {
+        return log;
+    }
 }
