b89b151c5ea08b8bba27ca98e4d36836227648ec
Thomas Angelini <thomas.angelini@studio.unibo.it>
2020-05-23 13:23:50 +0200

    Corrected some name

--- src/main/java/pcd/ass03/ex01/Main.java
+++ src/main/java/pcd/ass03/ex01/Main.java
@@ -1,11 +1,22 @@
 package pcd.ass03.ex01;
 
-import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import pcd.ass03.ex01.actors.GuiActor;
 
-public class Main {
-    public static void main(String[] args) throws Exception  {
+/**
+ * Class that start System.
+ */
+public final class Main {
 
+    private Main() { }
+
+    /**
+     * Start system.
+     * @param args, unused.
+     */
+    public static void main(final String[] args) {
+        final ActorSystem system = ActorSystem.create("MasterMind");
+        system.actorOf(Props.create(GuiActor.class), "GuiActor");
     }
 }
