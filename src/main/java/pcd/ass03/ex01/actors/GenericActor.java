4621766f7d75ade42bd3670d7e474f64d99a516d
Thomas Angelini <thomas.angelini@studio.unibo.it>
2020-05-23 10:16:07 +0200

    Create GenericActor

---
+++ src/main/java/pcd/ass03/ex01/actors/GenericActor.java
@@ -0,0 +1,21 @@
+package pcd.ass03.ex01.actors;
+
+import akka.actor.AbstractActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+public abstract class GenericActor extends AbstractActor {
+
+    /**
+     * Embedded Akka logger used to show information of the execution.
+     */
+    protected final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    /**
+     * If a message has not a known type, an error will be shown.
+     * @param message the non-recognized message.
+     */
+    protected final void messageNotRecognized(final Object message) {
+        log.error("Message not recognized: " + message);
+    }
+}
--- src/main/java/pcd/ass03/ex01/actors/GuiActor.java
+++ src/main/java/pcd/ass03/ex01/actors/GuiActor.java
@@ -1,10 +1,20 @@
 package pcd.ass03.ex01.actors;
 
-import akka.actor.AbstractActor;
+import pcd.ass03.ex01.messages.LogMsg;
 
-public class GuiActor extends AbstractActor {
+public final class GuiActor extends GenericActor {
+
+
     @Override
     public Receive createReceive() {
-        return null;
+        return receiveBuilder()
+                .match(LogMsg.class, this::handleLogMsg)
+                .matchAny(this::messageNotRecognized)
+                .build();
     }
+
+    private void handleLogMsg(LogMsg logMsg) {
+        logMsg.getLog();
+    }
+
 }
