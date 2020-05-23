39d752ecd3465b2fff7bf49737fa0ee3fab8228d
Thomas Angelini <thomas.angelini@studio.unibo.it>
2020-05-23 13:07:08 +0200

    Create GuiActor that start Game and Referee

--- src/main/java/pcd/ass03/ex01/actors/GuiActor.java
+++ src/main/java/pcd/ass03/ex01/actors/GuiActor.java
@@ -1,20 +1,84 @@
 package pcd.ass03.ex01.actors;
 
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import pcd.ass03.ex01.graphic.GameGui;
 import pcd.ass03.ex01.messages.LogMsg;
+import pcd.ass03.ex01.messages.LoseMsg;
+import pcd.ass03.ex01.messages.StartGameMsg;
+import pcd.ass03.ex01.messages.WinMsg;
 
+/**
+ * Actor that receive referee message and update Gui.
+ */
 public final class GuiActor extends GenericActor {
+
+    /**
+     * Reference to Gui's game.
+     */
+    private GameGui gameGui;
+
+    /**
+     * Reference to referee.
+     */
+    private ActorRef refereeActor;
+
+    @Override
+    public void preStart() {
+        this.gameGui = GameGui.of(this);
+        this.refereeActor = getContext().getSystem().actorOf(Props.create(RefereeActor.class),"Referee");
+    }
 
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(LogMsg.class, this::handleLogMsg)
+                .match(WinMsg.class, this::handleWinMsg)
+                .match(LoseMsg.class, this::handleLoseMsg)
                 .matchAny(this::messageNotRecognized)
                 .build();
     }
 
-    private void handleLogMsg(LogMsg logMsg) {
-        logMsg.getLog();
+    /**
+     * Send start Game to referee.
+     * @param nPlayers number of player in the game.
+     * @param combinationSize length of cypher.
+     */
+    public void sendStartGame(final int nPlayers, final int combinationSize) {
+        this.refereeActor.tell(new StartGameMsg(nPlayers, combinationSize), getSelf());
+    }
+
+
+    /**
+     * Handle to update Gui with the game winner.
+     * @param winMsg message from RefereeActor
+     */
+    private void handleWinMsg(final WinMsg winMsg) {
+        this.gameGui.finish(winMsg);
+    }
+
+    /**
+     * Handle to update Gui with the game winner.
+     * @param loseMsg message from RefereeActor.
+     */
+    private void handleLoseMsg(final LoseMsg loseMsg) {
+        if (loseMsg.getnActivePlayers() == 0) {
+            this.gameGui.finish(loseMsg);
+        } else {
+            // fixme check how to print his name
+            this.handleLogMsg(new LogMsg("Player " + loseMsg.getLoser() + " have lost!"));
+        }
+    }
+
+
+    /*
+     * This method will send a message to the GameGui.
+     * Message is an update of what happen in the game.
+     * @param logMsg.
+     */
+    private void handleLogMsg(final LogMsg logMsg) {
+        this.gameGui.update(logMsg.getLog());
     }
 
 }
